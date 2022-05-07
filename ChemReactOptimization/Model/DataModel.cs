0b0f402bcc4f4fddb9fb17ae105bda9781f5de22
takandrew <andreikaril@mail.ru>
2022-05-07 16:15:48 +0300

    Needed data is now displaying into program interface

--- ChemReactOptimization/Model/DataModel.cs
+++ ChemReactOptimization/Model/DataModel.cs
@@ -13,8 +13,13 @@
         public double G { get; set; } // Reaction mass consumption
         public double A { get; set; } // Reactor pressure
         public double N { get; set; } // Number of heat exchangers
+        public double T1Min { get; set; } // Coil temperature MIN
         public double T1 { get; set; } // Coil temperature
-        public double T2 { get; set; } // Diffuser temperature
+        public double T1Max { get; set; } // Coil temperature MAX
+        public double T2Min { get; set; } // Diffuser temperature MIN
+        public double T2 { get; set; } // Diffuser temperature 
+        public double T2Max { get; set; } // Diffuser temperature MAX
+        public double TSumMax { get; set; } // T1+T2 <= TSumMax
 
     }
 }
--- ChemReactOptimization/ViewModel/MainWindowViewModel.cs
+++ ChemReactOptimization/ViewModel/MainWindowViewModel.cs
@@ -1,14 +1,69 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+using ChemReactOptimization.Model;
+using WPF_MVVM_Classes;
 using ViewModelBase = ChemReactOptimization.Services.ViewModelBase;
 
 namespace ChemReactOptimization.ViewModel
 {
     public class MainWindowViewModel : ViewModelBase
     {
+
+        //private RelayCommand? _calculateCommand;
+        //private IEnumerable _dataList;
+        //private List<Point3D> _point3D = new();
+
+        private DataModel _dataModel = new DataModel()
+        {
+            Alpha = 1,
+            Beta = 1,
+            Mu = 1, 
+            Delta = 1,
+            G = 1,
+            A = 1, 
+            N = 2,
+            T1Min = -18,
+            T1Max = 7,
+            T2Min = -8, 
+            T2Max = 8,
+            TSumMax = 4
+        };
+
+        public DataModel DataModel
+        {
+            get => _dataModel;
+            set
+            {
+                _dataModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //public IEnumerable DataList
+        //{
+        //    get => _dataList;
+        //    set
+        //    {
+        //        _dataList = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+
+        //public RelayCommand CalculateCommand
+        //{
+        //    get
+        //    {
+        //        return _calculateCommand ??= new RelayCommand(c =>
+        //        {
+                    
+        //        });
+        //    }
+        //}
 
     }
 }
